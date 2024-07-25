352f8037d1cee3ec7cd0eedda37e0b8093461842
Viki <hi@viki.moe>
2024-07-25 14:26:51 +0800

    chore: init

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import react from '@vitejs/plugin-react-swc'
+import UnoCSS from 'unocss/vite'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [UnoCSS(), react()],
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  }
+})
