615ebb9015dde07b68f9501f21dc507973fe5280
Viki <hi@viki.moe>
2024-07-28 17:45:13 +0800

    chore: modify ui

--- src/pages/chat-list/hooks/use-chat-session.ts
+++ src/pages/chat-list/hooks/use-chat-session.ts
@@ -1,16 +1,19 @@
+import { chatListStore } from '../store'
 import { useActiveSession } from './use-active-session'
 import { useChatList } from './use-chat-list'
-import { chatListStore } from '../store'
 import { useEffect } from 'react'
 
 export function useChatSession() {
   const chatList = useChatList()
-  const activeSession = useActiveSession()
+  const session = useActiveSession()
 
-  const target = chatList.find((c) => c.type === activeSession?.type && c.id === activeSession?.id)
+  const target = chatList.find((c) => c.type === session?.type && c.id === session?.id)
 
   useEffect(() => {
     if (!target && chatList[0]) {
+      chatList[0].unreadCount = 0
+      chatList[0].chatting = true
+
       chatListStore.mutate.session = {
         id: chatList[0].id,
         type: chatList[0].type,
--- src/pages/chat-list/index.tsx
+++ src/pages/chat-list/index.tsx
@@ -88,7 +88,12 @@ export function ChatList() {
 
   return (
     <div className="flex gap-2">
-      <div className="pl-0 flex flex-col w-240px h-[calc(100vh-260px)] overflow-y-scroll">
+      <div
+        className={cn(
+          'pl-0 flex flex-col h-[calc(100vh-260px)] overflow-y-scroll',
+          list.length === 0 ? 'w-full' : 'w-240px',
+        )}
+      >
         {list.map((item) => {
           const isActive = item.id === session.id && item.type === session.type
           const lastMessage = item.history[item.history.length - 1]
@@ -129,108 +134,114 @@ export function ChatList() {
             </div>
           )
         })}
-        {list.length === 0 && <li className="h-full grid-center">No Chat Session</li>}
-      </div>
-
-      <div className="flex-1 overflow-hidden">
-        {session ? (
-          <>
-            <div className="flex flex-col flex-1">
-              <div className="flex rounded items-center gap-2 w-full p-2 bg-zinc-1/12">
-                <Avatar item={session} />
-                <div>
-                  {session.name}
-                  {session.type === 'group' ? ` (${session.info.member_count}/${session.info.max_member_count})` : ''}
-                </div>
-              </div>
-              <div ref={ref} className="w-full h-[calc(100vh-358px)] w-full overflow-scroll my-2">
-                {session.history.map((msg) => {
-                  const isSelf = msg.sender.user_id === info?.user_id
-
-                  return (
-                    <div key={msg.message_id} className="flex w-full gap-2 p-2">
-                      {!isSelf && <Avatar rounded item={{ type: 'private', id: msg.user_id }} />}
-                      <div className={cn('flex flex-col w-full', isSelf ? 'items-end' : '')}>
-                        <div className={cn('flex items-center gap-1 text-right')}>
-                          {!isSelf && <div className="font-bold">{msg.sender.nickname}</div>}
-                          <div className="text-gray/60 text-xs">
-                            {new Date(msg.time * 1000).toLocaleString('zh-CN')}
-                          </div>
-                          {isSelf && <div className="font-bold">{msg.sender.nickname}</div>}
-                        </div>
-                        <pre className="text-wrap mt-1 mb-0 font-sans">
-                          {msg.message.map((e: any, idx) => {
-                            switch (e.type) {
-                              case 'text':
-                                return <span key={`${e.type}-${idx}`}>{e.data.text}</span>
-                              case 'image':
-                                return (
-                                  <img
-                                    key={`${e.type}-${idx}`}
-                                    className="w-24 rounded"
-                                    src={e.data.url}
-                                    alt="chat-image"
-                                  />
-                                )
-                              case 'at':
-                                return (
-                                  <div
-                                    key={`${e.type}-${idx}`}
-                                    className="inline-flex flex-center gap-1 mx-1 bg-blue/20 text-blue-5 px-1 py-0.5 rounded"
-                                  >
-                                    <span>@</span>
-                                    <Avatar size="size-4" item={{ type: 'private', id: e.data.qq }} />
-                                  </div>
-                                )
-                              case 'face':
-                                return (
-                                  <img
-                                    key={`${e.type}-${idx}`}
-                                    className="h-5"
-                                    src={`/face/s${e.data.id}.gif`}
-                                    alt="face"
-                                  />
-                                )
-                              case 'json':
-                                return <span key={`${e.type}-${idx}`}>{JSON.parse(e.data.data).prompt}</span>
-                              case 'reply':
-                                return <span key={`${e.type}-${idx}`}>[reply:{e.data.id}]</span>
-                              case 'mface':
-                                return e.data.url ? (
-                                  <img
-                                    key={`${e.type}-${idx}`}
-                                    className="h-24 rounded"
-                                    src={e.data.url}
-                                    alt="m-face-image"
-                                  />
-                                ) : (
-                                  <span key={`${e.type}-${idx}`}>[mface:{e.data.id}]</span>
-                                )
-                              default:
-                                return <span key={`${e.type}-${idx}`}>{JSON.stringify(e)}</span>
-                            }
-                          })}
-                        </pre>
-                      </div>
-                      {isSelf && <Avatar rounded item={{ type: 'private', id: msg.user_id }} />}
-                    </div>
-                  )
-                })}
-              </div>
-            </div>
-            <div className="bottom-0 w-full flex items-center gap-2">
-              <Input {...msgInput.props} disabled={sendMsg.loading} onPressEnter={sendMsg.run} />
-              <Button onClick={sendMsg.run} loading={sendMsg.loading} className="flex items-center">
-                <span className="i-mdi-send" />
-              </Button>
-            </div>
-          </>
-        ) : (
-          <div className="flex-1 grid-center h-full">
-            <div>Place select a session on the left</div>
+        {list.length === 0 && (
+          <div className="h-full grid-center">
+            <div>No Chat Sessions, Select One from `Contact` Tab.</div>
           </div>
         )}
       </div>
+
+      {list.length !== 0 && (
+        <div className="flex-1 overflow-hidden">
+          {session ? (
+            <>
+              <div className="flex flex-col flex-1">
+                <div className="flex rounded items-center gap-2 w-full p-2 bg-zinc-1/12">
+                  <Avatar item={session} />
+                  <div>
+                    {session.name}
+                    {session.type === 'group' ? ` (${session.info.member_count}/${session.info.max_member_count})` : ''}
+                  </div>
+                </div>
+                <div ref={ref} className="w-full h-[calc(100vh-358px)] w-full overflow-scroll my-2">
+                  {session.history.map((msg) => {
+                    const isSelf = msg.sender.user_id === info?.user_id
+
+                    return (
+                      <div key={msg.message_id} className="flex w-full gap-2 p-2">
+                        {!isSelf && <Avatar rounded item={{ type: 'private', id: msg.user_id }} />}
+                        <div className={cn('flex flex-col w-full', isSelf ? 'items-end' : '')}>
+                          <div className={cn('flex items-center gap-1 text-right')}>
+                            {!isSelf && <div className="font-bold">{msg.sender.nickname}</div>}
+                            <div className="text-gray/60 text-xs">
+                              {new Date(msg.time * 1000).toLocaleString('zh-CN')}
+                            </div>
+                            {isSelf && <div className="font-bold">{msg.sender.nickname}</div>}
+                          </div>
+                          <pre className="text-wrap mt-1 mb-0 font-sans">
+                            {msg.message.map((e: any, idx) => {
+                              switch (e.type) {
+                                case 'text':
+                                  return <span key={`${e.type}-${idx}`}>{e.data.text}</span>
+                                case 'image':
+                                  return (
+                                    <img
+                                      key={`${e.type}-${idx}`}
+                                      className="w-24 rounded"
+                                      src={e.data.url}
+                                      alt="chat-image"
+                                    />
+                                  )
+                                case 'at':
+                                  return (
+                                    <div
+                                      key={`${e.type}-${idx}`}
+                                      className="inline-flex flex-center gap-1 mx-1 bg-blue/20 text-blue-5 px-1 py-0.5 rounded"
+                                    >
+                                      <span>@</span>
+                                      <Avatar size="size-4" item={{ type: 'private', id: e.data.qq }} />
+                                    </div>
+                                  )
+                                case 'face':
+                                  return (
+                                    <img
+                                      key={`${e.type}-${idx}`}
+                                      className="h-5"
+                                      src={`/face/s${e.data.id}.gif`}
+                                      alt="face"
+                                    />
+                                  )
+                                case 'json':
+                                  return <span key={`${e.type}-${idx}`}>{JSON.parse(e.data.data).prompt}</span>
+                                case 'reply':
+                                  return <span key={`${e.type}-${idx}`}>[reply:{e.data.id}]</span>
+                                case 'mface':
+                                  return e.data.url ? (
+                                    <img
+                                      key={`${e.type}-${idx}`}
+                                      className="h-24 rounded"
+                                      src={e.data.url}
+                                      alt="m-face-image"
+                                    />
+                                  ) : (
+                                    <span key={`${e.type}-${idx}`}>[mface:{e.data.id}]</span>
+                                  )
+                                default:
+                                  return <span key={`${e.type}-${idx}`}>{JSON.stringify(e)}</span>
+                              }
+                            })}
+                          </pre>
+                        </div>
+                        {isSelf && <Avatar rounded item={{ type: 'private', id: msg.user_id }} />}
+                      </div>
+                    )
+                  })}
+                </div>
+              </div>
+              <div className="bottom-0 w-full flex items-center gap-2">
+                <Input {...msgInput.props} disabled={sendMsg.loading} onPressEnter={sendMsg.run} />
+                <Button onClick={sendMsg.run} loading={sendMsg.loading} className="flex items-center">
+                  <span className="i-mdi-send" />
+                </Button>
+              </div>
+            </>
+          ) : (
+            <div className="flex-1 grid-center h-full">
+              <div>Place select a session on the left</div>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   )
 }
