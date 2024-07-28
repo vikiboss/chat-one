9ed762289c7abd1fd510778337c2029b6728f2bb
Viki <hi@viki.moe>
2024-07-28 17:35:50 +0800

    fix: fix 0

--- src/pages/chat-list/hooks/use-chat-session.ts
+++ src/pages/chat-list/hooks/use-chat-session.ts
@@ -1,9 +1,22 @@
 import { useActiveSession } from './use-active-session'
 import { useChatList } from './use-chat-list'
+import { chatListStore } from '../store'
+import { useEffect } from 'react'
 
 export function useChatSession() {
   const chatList = useChatList()
   const activeSession = useActiveSession()
 
-  return chatList.find((c) => c.type === activeSession?.type && c.id === activeSession?.id) ?? chatList[0]
+  const target = chatList.find((c) => c.type === activeSession?.type && c.id === activeSession?.id)
+
+  useEffect(() => {
+    if (!target && chatList[0]) {
+      chatListStore.mutate.session = {
+        id: chatList[0].id,
+        type: chatList[0].type,
+      }
+    }
+  }, [target, chatList[0]])
+
+  return target ?? chatList[0]
 }
