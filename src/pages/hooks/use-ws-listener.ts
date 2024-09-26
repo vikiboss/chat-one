d3ef02b721f71c5f521963f7f3032bb94c6397bb
Viki <hi@viki.moe>
2024-09-26 16:36:34 +0800

    fix: fix reload

--- src/pages/hooks/use-ws-listener.ts
+++ src/pages/hooks/use-ws-listener.ts
@@ -5,63 +5,67 @@ import { homeStore } from '../store'
 import { blackList } from '@/utils/blacklist'
 
 export function useWsListener() {
-  useEventListener(wsApi.instance, 'message', (message: MessageEvent) => {
-    const msg = JSON.parse(message.data)
+  useEventListener(
+    () => wsApi.instance,
+    'message',
+    (message: MessageEvent) => {
+      const msg = JSON.parse(message.data)
 
-    const isInBlacklist = blackList.some((id) => msg.user_id === id)
+      const isInBlacklist = blackList.some((id) => msg.user_id === id)
 
-    switch (msg.post_type) {
-      case 'message': {
-        const target = homeStore.mutate.contactList.find((c) => {
-          const isGroup = msg.message_type === 'group'
-          return isGroup ? c.id === msg.group_id : c.id === msg.user_id
-        })
+      switch (msg.post_type) {
+        case 'message': {
+          const target = homeStore.mutate.contactList.find((c) => {
+            const isGroup = msg.message_type === 'group'
+            return isGroup ? c.id === msg.group_id : c.id === msg.user_id
+          })
 
-        const { id, type } = chatListStore.mutate.session || {}
+          const { id, type } = chatListStore.mutate.session || {}
 
-        if (target) {
-          if (id === target.id && type === target.type) {
-            target.unreadCount = 0
-            target.chatting = true
-          } else {
-            !isInBlacklist && target.unreadCount++
-            homeStore.mutate.contactList.splice(homeStore.mutate.contactList.indexOf(target), 1)
-            homeStore.mutate.contactList.unshift(target)
-          }
-
-          const idx = msg.message.findIndex((e: any) => e.type === 'reply')
-
-          if (idx !== -1 && msg.message.length > idx + 1) {
-            const atItem = msg.message[idx + 1]
-
-            if (atItem.type === 'at') {
-              msg.message.splice(idx + 1, 1)
-              msg.message[idx].data.__user_id__ = atItem.data.qq
+          if (target) {
+            if (id === target.id && type === target.type) {
+              target.unreadCount = 0
+              target.chatting = true
+            } else {
+              !isInBlacklist && target.unreadCount++
+              homeStore.mutate.contactList.splice(homeStore.mutate.contactList.indexOf(target), 1)
+              homeStore.mutate.contactList.unshift(target)
             }
+
+            const idx = msg.message.findIndex((e: any) => e.type === 'reply')
+
+            if (idx !== -1 && msg.message.length > idx + 1) {
+              const atItem = msg.message[idx + 1]
+
+              if (atItem.type === 'at') {
+                msg.message.splice(idx + 1, 1)
+                msg.message[idx].data.__user_id__ = atItem.data.qq
+              }
+            }
+
+            target.history.push(msg)
           }
 
-          target.history.push(msg)
+          break
         }
-
-        break
-      }
-      case 'notice':
-        break
-      case 'request':
-        break
-      case 'meta_event': {
-        switch (msg.meta_event_type) {
-          case 'heartbeat': {
-            globalStore.mutate.isOnline = msg.status.online
-            break
+        case 'notice':
+          break
+        case 'request':
+          break
+        case 'meta_event': {
+          switch (msg.meta_event_type) {
+            case 'heartbeat': {
+              globalStore.mutate.isOnline = msg.status.online
+              break
+            }
+            default:
+              break
           }
-          default:
-            break
+          break
         }
-        break
+        default:
+          break
       }
-      default:
-        break
-    }
-  })
+    },
+  )
 }
